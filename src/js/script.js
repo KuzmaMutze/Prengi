eb67b3ae611a3057145e2eb5ff6091c313ab8def
Matvey <r3dw0r13@yandex.ru>
2020-01-23 16:10:38 +0400

    commit

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        nav: true,
+        navText : ["",""],
+        // autoplay: true,
+        autoplayTimeout:6000,
+        autoplayHoverPause: true,
+        items:1,
+        smartSpeed:1050,
+        margin:1000
+    });
+});
