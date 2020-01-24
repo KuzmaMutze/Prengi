e594849ffc271f1cde42f48ba0c591b9f92dc860
Matvey <r3dw0r13@yandex.ru>
2020-01-24 21:41:37 +0400

    carusel two

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,11 +3,17 @@ $(document).ready(function(){
         loop:true,
         nav: true,
         navText : ["",""],
-        // autoplay: true,
+        autoplay: true,
         autoplayTimeout:6000,
         autoplayHoverPause: true,
         items:1,
         smartSpeed:1050,
         margin:1000
+    });
+    $(".decision__carousel").owlCarousel({
+        navText: [
+            '<span class="arrow-owl arrow-left"></span>',
+            '<span class="arrow-owl arrow-right"></span>'
+        ]
     });
 });
